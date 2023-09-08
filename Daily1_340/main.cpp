46f97e60a2cdc8fe83f1e94b1843623a8092367a
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-09-08 16:28:27 -0500

    Add documentation box

--- Daily1_340/main.cpp
+++ Daily1_340/main.cpp
@@ -1,9 +1,14 @@
-//
-//  main.cpp
-//  Daily1_340
-//
-//  Created by scott sheble on 9/8/23.
-//
+/**********************************************************************************************
+ * NIU CSCI 340 Section 1                                                                     *
+ * Daily 1                                                                                    *
+ * Scott Sheble - Z1963374 - scottsheble                                                      *
+ *                                                                                            *
+ *   I certify that everything I am submitting is either provided by the professor for use in *
+ * the assignment, or work done by me personally. I understand that if I am caught submitting *
+ * the work of others (including StackOverflow or ChatGPT) as my own is an act of Academic    *
+ * Misconduct and will be punished as such.                                                   *
+ *                                                                                            *
+ **********************************************************************************************/
 
 #include <iostream>
 
