fe5f147914f05e3203e639968b4bec4ffc6ada47
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-09-08 16:26:00 -0500

    Initial Commit

---
+++ Daily1_340/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Daily1_340
+//
+//  Created by scott sheble on 9/8/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
